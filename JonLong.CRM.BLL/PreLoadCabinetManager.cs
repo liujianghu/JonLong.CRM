a836302f8e7e024880a7e894d00f472fdcdaf758
Lake.liu <liujianghu2010@gmail.com>
2014-09-22 20:55:14 +0800

    添加预装柜基础代码

---
+++ JonLong.CRM.BLL/PreLoadCabinetManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JonLong.CRM.Models;
+using JonLong.CRM.DAL;
+
+namespace JonLong.CRM.BLL
+{
+    public class PreLoadCabinetManager
+    {
+        protected PreLoadCabinetManager() { }
+
+        public static readonly PreLoadCabinetManager Instance = new PreLoadCabinetManager();
+
+        public  List<PreLoadCabinet> LoadAviailable(string customerCode)
+        {
+            return PreLoadCabinetDataProvider.LoadAviailable(customerCode);
+        }
+
+        public  void Insert(PreLoadCabinet cabinet)
+        {
+            PreLoadCabinetDataProvider.Insert(cabinet);
+        }
+
+        public decimal GetFilled(string customerCode, string guid, string containerNo)
+        {
+            return PreLoadCabinetDataProvider.GetFilled(customerCode, guid, containerNo);
+        }
+
+    }
+}
---
+++ JonLong.CRM.Web/Controllers/PreLoadCabinetController.cs
@@ -0,0 +1,20 @@
+﻿using JonLong.CRM.Web.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JonLong.CRM.Models;
+using JonLong.CRM.BLL;
+
+namespace JonLong.CRM.Web.Controllers
+{
+    public class PreLoadCabinetController : Controller
+    {
+        [RoleAuthorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
